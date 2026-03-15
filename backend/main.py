99ac8b9e02739aae06e55e4665c7bc76f15401ef
Daniel Pavela <S4170242@student.rmit.edu.au>
2026-03-15 11:21:14 +1100

    Fix middleware connection issues

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 import agent
 import item
+from fastapi.middleware.cors import CORSMiddleware
 
 # Setup app
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Add listener for message POST request
 @app.post("/message/")
